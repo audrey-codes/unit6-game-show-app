30dbd0bd5bde16da8193af805a8248a0b2762539
Audrey Bazyk <audreybazyk@gmail.com>
2020-07-09 14:25:15 -0500

    Fixed start button
    Added forgotten '' to start button display 'none' and created random phrases array

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const qwerty = document.getElementById('qwerty');
-  const phrase = document.getElementById('phrase');
+  const phrase = document.querySelector('#phrase ul');
   const startButton = document.querySelector('.btn__reset');
   const overlay = document.getElementById('overlay');
 
   let missed = 0;
 
+// START BUTTON
   startButton.addEventListener('click', () => {
-    overlay.style.display = none;
+    overlay.style.display = 'none';
   });
 
 // PHRASES
   let phrases = [
     "for the birds",
     "a different kettle of fish",
-    "a drop in the ocean",
     "bet your bottom dollar",
+    "a drop in the ocean",
     "for all intents and purposes"
   ]
+
+// GET RANDOM PHRASE FUNCTION
+  function getRandomPhraseAsArray(arr) {
+    let randomPhrase = arr[Math.floor(Math.random() * arr.length)];
+    let words = randomPhrase.split("");
+    return words;
+  }
 
 });
